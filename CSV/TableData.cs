ffbbbf16c4d35a8d5a47da385a5cbc1f2cc33240
Alex Liu <63307936+Skyrim07@users.noreply.github.com>
2021-07-29 09:42:31 +0800

    Add files via upload

---
+++ CSV/TableData.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SKCell
+{
+    public class TableData : MonoSingleton<TableData>
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+            LoadTable();
+        }
+
+        private const string CSVPATH = "CSV";
+
+        /// <summary>
+        /// 添加CSV表.
+        /// </summary>
+        public void LoadTable()
+        {
+            List<string> tableList = new List<string>();
+            var tables = Resources.LoadAll(CSVPATH);
+            if (tables != null)
+            {
+                foreach (var table in tables)
+                {
+                    TextAsset textAsset = table as TextAsset;
+                    if (textAsset != null)
+                    {
+                        tableList.Add(textAsset.text);
+                    }
+                }
+            }
+            CommonUtils.EditorLogNormal("CSV Table count:" + tableList.Count);
+            TableAgent.instance.Add(tableList);
+        }
+    }
+}

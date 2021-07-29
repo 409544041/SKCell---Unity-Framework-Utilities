2b7bd20b68b2d4c91974bcd927cb44047712254c
Alex Liu <63307936+Skyrim07@users.noreply.github.com>
2021-07-29 09:44:56 +0800

    Add files via upload

---
+++ Effects/SKPostProcessManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+namespace SKCell {
+    public class SKPostProcessManager : Singleton<SKPostProcessManager>
+    {
+        //Procedure variable
+        private static float f = 0;
+
+        public static void StartContrastProcedure(PostProcessVolume volume, float targetValue, float time)
+        {
+            ColorGrading setting = volume.profile.GetSetting<ColorGrading>();
+
+            CommonUtils.StartProcedure(ProcedureType.Linear, f, setting.contrast, targetValue, time, (f) =>
+            {
+                setting.contrast.value = f;
+            }, (f) =>
+            {
+                setting.contrast.value = targetValue;
+            }
+            );
+        }
+        public static void StartBrightnessProcedure(PostProcessVolume volume, float targetValue, float time)
+        {
+            ColorGrading setting = volume.profile.GetSetting<ColorGrading>();
+
+            CommonUtils.StartProcedure(ProcedureType.Linear, f, setting.brightness, targetValue, time, (f) =>
+            {
+                setting.brightness.value = f;
+            }, (f) =>
+            {
+                setting.brightness.value = targetValue;
+            }
+            );
+        }
+
+        public static void StartSaturationProcedure(PostProcessVolume volume, float targetValue, float time)
+        {
+            ColorGrading setting = volume.profile.GetSetting<ColorGrading>();
+
+            CommonUtils.StartProcedure(ProcedureType.Linear, f, setting.saturation, targetValue, time, (f) =>
+            {
+                setting.saturation.value = f;
+            }, (f) =>
+             {
+                 setting.saturation.value = targetValue;
+             }
+            );
+        }
+
+    }
+}
---
+++ Physics/SKSceneObjectResponder.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace SKCell
+{
+    [RequireComponent(typeof(Collider))]
+    
+    public class SKSceneObjectResponder : MonoBehaviour
+    {
+        public bool isMouseOver = false;
+
+        public UnityEvent onMouseEnter, onMouseOver, onMouseExit, onMouseDown, onMouseUp, onMouseDrag;
+
+
+        private void OnMouseOver()
+        {
+            onMouseOver.Invoke();
+        }
+        private void OnMouseExit()
+        {
+            isMouseOver = false;
+            onMouseExit.Invoke();
+        }
+
+        private void OnMouseDown()
+        {
+            onMouseDown.Invoke();
+        }
+
+        private void OnMouseUp()
+        {
+            onMouseUp.Invoke();
+        }
+
+        private void OnMouseEnter()
+        {
+            isMouseOver = true;
+            onMouseEnter.Invoke();
+        }
+
+        private void OnMouseDrag()
+        {
+            onMouseDrag.Invoke();
+        }
+    }
+}
